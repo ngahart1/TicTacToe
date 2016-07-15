d743644c1638b5df8e8fe5069efe4d2b588e8d35
Neil Gahart <ngahart1@ugradx.cs.jhu.edu>
2016-07-14 21:09:21 -0400

    allowed for gameplay in main

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -6,8 +6,20 @@ using namespace std;
 int main () {
   cout << "Welcome to TicTacToe!" << endl;
   int p1_type, p2_type;
-  cout << "Player 1, what is your type?" << endl;
+  /*cout << "Player 1, what is your type?" << endl;
   cin >> p1_type;
-  cout << "p1_type is " << p1_type << endl;
+  cout << "p1_type is " << p1_type << endl;*/
+  TicTacToe ttt;
+  ttt.print();
+  int final_result = ttt.play();
+  if (final_result == PLAYER1) {
+    cout << "Congratulations Player 1, you have won!" << endl;
+  }
+  else if (final_result == PLAYER2) {
+    cout << "Congratulations Player 2, you have won!" << endl;
+  }
+  else if (final_result == DRAW) {
+    cout << "Draw" << endl;
+  }
   return 0;
 }
