601a2a9ca42120d0e0d2d23bda63d0079d58ebea
Neil Gahart <ngahart1@ugradx.cs.jhu.edu>
2016-07-14 21:24:46 -0400

    Works, except can override move

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -32,23 +32,24 @@ int TicTacToe::makeMove() {
   int first, second;
   std::cin >> first >> second;
   board[first][second] = whose_turn;
-
+  whose_turn = 3-whose_turn; //maps 1 to 2 and 2 to 1
+  
   //check win in a row or column
   for (int r = 0; r < 3; r++) {
-    if (board[r][0] == board[r][1] && board[r][0] == board[r][2] && board[r][1] == board[r][2]) {
+    if (board[r][0] != 0 && board[r][0] == board[r][1] && board[r][0] == board[r][2] && board[r][1] == board[r][2]) {
       if (board[r][0] == 1) {return PLAYER1;}
       else {return PLAYER2;}
     }
-    if (board[0][r] == board[1][r] && board[0][r] == board[2][r] && board[1][r] == board[2][r]) {
+    if (board[0][r] != 0 && board[0][r] == board[1][r] && board[0][r] == board[2][r] && board[1][r] == board[2][r]) {
       if (board[0][r] == 1) {return PLAYER1;}
       else {return PLAYER2;}
     }
   }
-  if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[1][1] == board[2][2]) {
+  if (board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[1][1] == board[2][2]) {
     if (board[0][0] == 1) {return PLAYER1;}
     else {return  PLAYER2;}
   }
-  if (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[1][1] == board[0][2]) {
+  if (board[2][0] != 0 && board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[1][1] == board[0][2]) {
     if (board[1][1] == 1) {return PLAYER1;}
     else {return PLAYER2;}
   }
@@ -56,11 +57,12 @@ int TicTacToe::makeMove() {
   //at this stage, we know there is no winner, must check for draw
   for (int row = 0; row < 3; row++) {
     for (int col = 0; col < 3; col++) {
-      if (board[row][col] != 0) {
-	return KEEP_PLAYING;
+      if (board[row][col] == 0) {
+	return KEEP_PLAYING; //there is some square that is empty
       }
     }
   }
+  return DRAW;
 }
 
 
